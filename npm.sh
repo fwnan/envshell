3a4d488d5af3ef61420c75862521766a0c1e9d50
fwnan <71653989+fwnan@users.noreply.github.com>
2022-12-27 23:13:50 +0800

    Update npm.sh

--- npm.sh
+++ npm.sh
@@ -3,20 +3,23 @@
 #安装目录和压缩包下载目录
 INSTALL_PATH="/usr/local/node"
 
+#Node版本信息
+DOWN-VSN=latest-v18.x/node-v18.12.1.tar.xz
+NODE-VSN=node-v18.12.1
+
 #创建目录结构
 mkdir -p ${INSTALL_PATH}
 
 #下载Node国内镜像到指定目录
-wget -P ${INSTALL_PATH} https://npmmirror.com/mirrors/node/v16.18.1/node-v16.18.1-linux-x64.tar.xz
+wget -P ${INSTALL_PATH} https://registry.npmmirror.com/-/binary/node/${DOWN-VSN}
 
-#解压成tar-> tar文件解压成文件夹 -> 更名
-xz -d ${INSTALL_PATH}/node-v16.18.1-linux-x64.tar.xz 
-tar -xvf ${INSTALL_PATH}/node-v16.18.1-linux-x64.tar
-mv ${INSTALL_PATH}/node-v16.18.1-linux-x64 node-v16.18
+#1.解压成tar 2.tar文件解压成文件夹
+xz -d ${INSTALL_PATH}/${NODE-VSN}.tar.xz 
+tar -xvf ${INSTALL_PATH}/${NODE-VSN}.tar
 
 #配置软链接, 全局可使用命令
-ln -s ${INSTALL_PATH}/node-v16.18/bin/node /usr/bin/node
-ln -s ${INSTALL_PATH}/node-v16.18/bin/npm /usr/bin/npm
+ln -s ${INSTALL_PATH}/${NODE-VSN}/bin/node /usr/bin/node
+ln -s ${INSTALL_PATH}/${NODE-VSN}/bin/npm /usr/bin/npm
 
 #查询node版本和npm版本
 node --version && npm --version
