c69c10635ae51750ebf221b91e96a8fbc7733b3c
fwnan <71653989+fwnan@users.noreply.github.com>
2022-12-28 00:57:39 +0800

    Update npm.sh

--- npm.sh
+++ npm.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
 #安装目录和压缩包下载目录
-INSTALL_PATH="/usr/local/node"
-
-#Node版本信息
-DOWN_VSN="latest-v18.x/node-v18.12.1-linux-x64.tar.xz"
-NODE_VSN="node-v18.12.1-linux-x64"
+#注: /usr/local目录需要配置环境变量才有权限执行命令,最好选择/opt或者/usr
+INSTALL_PATH="/opt/node"
 
 #创建目录结构
 mkdir -p ${INSTALL_PATH}
 
+DOWNLOAD=node/latest-v18.x/node-v18.12.1-linux-x64.tar.xz
+VERSION=node-v18.12.1-linux-x64
+
 #下载Node国内镜像到指定目录
-wget -P ${INSTALL_PATH} https://registry.npmmirror.com/-/binary/node/${DOWN_VSN}
+wget -P ${INSTALL_PATH} https://registry.npmmirror.com/-/binary/${DOWNLOAD}
 
 #1.解压成tar 2.tar文件解压成文件夹
-xz -d ${INSTALL_PATH}/${NODE_VSN}.tar.xz 
-tar -xvf ${INSTALL_PATH}/${NODE_VSN}.tar
+cd ${INSTALL_PATH}
+xz -d ${VERSION}.tar.xz 
+tar -xvf ${VERSION}.tar 
 
 #配置软链接, 全局可使用命令
-ln -s ${INSTALL_PATH}/${NODE_VSN}/bin/node /usr/bin/node
-ln -s ${INSTALL_PATH}/${NODE_VSN}/bin/npm /usr/bin/npm
+ln -s ${INSTALL_PATH}/${VERSION}/bin/node /usr/bin/node
+ln -s ${INSTALL_PATH}/${VERSION}/bin/npm /usr/bin/npm
 
 #查询node版本和npm版本
 node --version && npm --version
 
 #NPM设置为阿里镜像源(官方地址: https://registry.npmjs.org/)
-npm config set registry http://registry.npmmirror.com
+npm config set registry http://registry.npmmirror.com --global
 
 #验证镜像源地址
 npm config get registry
