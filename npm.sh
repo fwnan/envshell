e23071fdd058186035a875b2d5d012feff0de306
gnef <71653989+fwnan@users.noreply.github.com>
2023-07-09 11:23:58 +0800

    Update npm.sh

--- npm.sh
+++ npm.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
-#安装目录和压缩包下载目录, 注: /usr/local目录需要配置环境变量才有权限执行命令,最好选择/opt或者/usr
+#安装目录. 注: /usr/local目录需要配置环境变量才有权限执行命令,最好选择/opt或者/usr
 INSTALL_PATH="/opt/nodejs"
+#下载地址, NODE最终安装名
+NODEJS_URL="https://registry.npmmirror.com/-/binary/node/v20.4.0/node-v20.4.0-linux-x64.tar.xz"
+NODE_NAME="node-v20.4.0-linux-x64"
 
-#nodejs下载地址
-WGET_NODE_URL=https://registry.npmmirror.com/-/binary/node/v20.4.0/node-v20.4.0-linux-x64.tar.xz
+#目录不存在,创建指定目录,否则移除
+if [ ! -d "${INSTALL_PATH}" ]; then
+  mkdir -p ${INSTALL_PATH}
+else
+  rm -rf ${INSTALL_PATH}
+fi
 
-#node version
-VERSION=node-v20.4.0-linux-x64
-
-#创建目录
-mkdir -p ${INSTALL_PATH}
-#下载Node到指定目录
-wget -P ${INSTALL_PATH} ${WGET_NODE_URL}
+#下载Nodejs
+wget -P ${INSTALL_PATH} ${NODEJS_URL}
 
 cd ${INSTALL_PATH}
 #1.解压成tar 2.tar文件解压成文件夹
-xz -d ${VERSION}.tar.xz && tar -xvf ${VERSION}.tar
+xz -d ${NODE_NAME}.tar.xz && tar -xvf ${NODE_NAME}.tar
+
+#删除压缩包
+find . -type f \( -name "*.tar" -o -name "*.xz" \) -delete
 
 #配置软链接,全局可用命令
-ln -sf ${INSTALL_PATH}/${VERSION}/bin/node /usr/bin/node
-ln -sf ${INSTALL_PATH}/${VERSION}/bin/npm /usr/bin/npm
+ln -sf ${INSTALL_PATH}/${NODE_NAME}/bin/node /usr/bin/node
+ln -sf ${INSTALL_PATH}/${NODE_NAME}/bin/npm /usr/bin/npm
 
 #查询node版本和npm版本
 node -v && npm -v
-
 #npm设置阿里镜像源(https://registry.npmjs.org)
 npm config set registry http://registry.npmmirror.com --global
-
 #验证镜像源地址
 npm config get registry
