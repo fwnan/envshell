ae7e802083177bc6c8f77bfec27cea0003546794
fwnan <71653989+fwnan@users.noreply.github.com>
2024-03-03 17:18:17 +0800

    maven version => 3.9.6

--- maven.sh
+++ maven.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
-#安装目录
-INSTALL_PATH="/opt/maven"
-#下载地址,压缩包,maven最终目录名
-MAVEN_URL="https://dlcdn.apache.org/maven/maven-3/3.9.3/binaries/apache-maven-3.9.3-bin.tar.gz"
-MAVEN_TAR="apache-maven-3.9.3-bin.tar.gz"
-MAVEN_DIR="apache-maven-3.9.3"
 
-#目录不存在,创建指定目录. 否则移除之前的Maven目录
+# install dir
+INSTALL_PATH="/opt/maven"
+
+# maven zip
+MAVEN_URL="https://archive.apache.org/dist/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz"
+MAVEN_TAR="apache-maven-3.9.6-bin.tar.gz"
+MAVEN_DIR="apache-maven-3.9.6"
+
+# 安装路径不存在则新建, 存在则清空目录数据
 if [ ! -d "${INSTALL_PATH}" ]; then
   mkdir -p ${INSTALL_PATH}
 else
-  rm -rf ${INSTALL_PATH}
+  rm -rf ${INSTALL_PATH}/*
 fi
 
-#下载Maven
-wget -P ${INSTALL_PATH} ${MAVEN_URL}
-#解压指定目录的maven压缩包
-tar -zxvf ${INSTALL_PATH}/${MAVEN_TAR} -C ${INSTALL_PATH} > /dev/null 2>&1
+cd ${INSTALL_PATH} 
+
+#下载maven到安装目录, 原地解压
+wget ${MAVEN_URL} && tar -zxvf ${MAVEN_TAR} -C ${INSTALL_PATH} > /dev/null 2>&1
+#wget -P ${INSTALL_PATH} ${MAVEN_URL} && tar -zxvf ${INSTALL_PATH}/${MAVEN_TAR} -C ${INSTALL_PATH} > /dev/null 2>&1
 #移除下载包
-mv ${INSTALL_PATH}/${MAVEN_TAR} /dev/null
+rm ${MAVEN_TAR}
 
 #重定向输出环境变量文件
 echo "export MAVEN_HOME=${INSTALL_PATH}/${MAVEN_DIR}" >> /etc/profile
 echo "export PATH=\$PATH:\$MAVEN_HOME/bin" >> /etc/profile
+
+#更新环境变量值
+sudo sed -i 's#\(export MAVEN_HOME=\).*#\1${INSTALL_PATH}/${MAVEN_DIR}#' /etc/profile
+
+#sudo sed -i 's/MAVEN_HOME=".*"/MAVEN_HOME="'"${INSTALL_PATH}/${MAVEN_DIR}"'"/g' /etc/source
+#sudo sed -i 's/PATH=".*"/PATH="'"$PATH:$MAVEN_HOME/bin"'"/g' /etc/source
+
 #刷新环境变量
 source /etc/profile
-echo "===Maven版本信息==="
+echo "==============MAVEN VERSION=============="
 mvn -v
-#执行bash,让脚本环境与外部linux一致
 bash
